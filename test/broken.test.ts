912b727a78f425fdfd268ec494efed753c67e1ab
reinaldocosta <reinaldo.costa@gentrop.com>
2023-10-05 16:02:57 -0300

    resolve test

--- test/broken.test.ts
+++ test/broken.test.ts
@@ -2,7 +2,7 @@ import { it } from 'node:test'
 import assert from 'node:assert'
 
 it('calls a nonexistent method', () => {
-  const actual = 1
+  const actual = 2
   const expected = 2
   assert.equal(actual, expected)
 })

ac88936db56340c1a43efee66108160f0d013718
reinaldocosta <reinaldo.costa@gentrop.com>
2023-10-05 15:48:49 -0300

    initial

---
+++ test/pass.test.ts
@@ -0,0 +1,8 @@
+import { it } from 'node:test'
+import assert from 'node:assert'
+
+it('calls a nonexistent method', () => {
+	const actual = 1
+	const expected = 1
+	assert.equal(actual, expected)
+})

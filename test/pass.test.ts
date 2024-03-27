154472a27a26d862fcfdb563a15e5c0c202edbfc
Lint Action <lint-action@samuelmeuli.com>
2024-03-27 16:56:42 +0000

    fix: fix code style issues with ESLint

--- test/pass.test.ts
+++ test/pass.test.ts
@@ -2,7 +2,7 @@ import { it } from 'node:test'
 import assert from 'node:assert'
 
 it('calls a nonexistent method', () => {
-	const actual = 1
-	const expected = 1
-	assert.equal(actual, expected)
+  const actual = 1
+  const expected = 1
+  assert.equal(actual, expected)
 })

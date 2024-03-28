1b02acc9baf998997b450ca37fdf05884f1d105b
Reinaldo Costa <reinaldo.costa@gentrop.com>
2024-03-28 12:22:30 -0300

    feat: sample dockerfile

--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
+import { createServer } from 'node:http'
+
 console.log('hello world')
 const unused = 'It will generate an PR annotation'
+const host = 'localhost'
+const port = 8080
+
+const server = createServer((req, res) => {
+  res.write('hello world')
+  res.end()
+})
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
